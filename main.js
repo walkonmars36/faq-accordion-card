496b211fdb17bb95fd6a3bb3feee03107c97cb3d
Mark Lawson <mark@walkonmars.co.uk>
2023-02-21 16:05:04 +0000

    individual answers display toggling

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+const questions = document.querySelectorAll(".card__fact-section");
 
+function handleQuestionClick(e) {
+  const question = e.currentTarget;
+  const answer = question.nextElementSibling;
+  const isActive = question.classList.contains("active");
+
+  // remove active class from question and toggle-answer class from answer paragraph
+  questions.forEach((question) => {
+    question.classList.remove("active");
+    question.nextElementSibling.classList.remove("display-answer");
+  });
+
+  // then display selected answer
+  if (!isActive) {
+    question.classList.add("active");
+    answer.classList.add("display-answer");
+  }
+}
+
+questions.forEach((question) => question.addEventListener("click", handleQuestionClick));

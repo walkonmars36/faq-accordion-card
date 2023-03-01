56dde0cd2a170a9df55fd77da1a013976234ba59
Mark Lawson <mark@walkonmars.co.uk>
2023-03-01 13:46:41 +0000

    desktop images not hidden at mobile size

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
 const questions = document.querySelectorAll(".card__fact-section");
+const answers = document.querySelectorAll(".card__fact-answer");
+
+function closeAllAnswers() {
+  answers.forEach((answer) => {
+    answer.style.maxHeight = null;
+    answer.style.transition = "max-height 0.2s ease";
+    answer.style.marginTop = null;
+    answer.style.opacity = 0;
+  });
+  questions.forEach((question) => question.setAttribute("aria-expanded", false));
+}
 
 function handleQuestionClick(e) {
   const question = e.currentTarget;
   const answer = question.nextElementSibling;
-  const isActive = question.classList.contains("active");
+  const expanded = question.getAttribute("aria-expanded") === "true";
 
-  // remove active class from question and toggle-answer class from answer paragraph
-  questions.forEach((question) => {
-    question.classList.remove("active");
-    question.nextElementSibling.style.maxHeight = null;
-  });
+  if (!expanded) {
+    closeAllAnswers();
+  }
 
-  // then display selected answer
-  if (!isActive) {
-    question.classList.add("active");
+  question.setAttribute("aria-expanded", !expanded);
+  if (expanded) {
+    // If section was expanded, collapse it
+    answer.style.maxHeight = null;
+    answer.style.transition = "max-height 0.2s ease";
+    answer.style.marginTop = null;
+    answer.style.opacity = 0;
+  } else {
+    // If section was collapsed, expand it
     answer.style.maxHeight = answer.scrollHeight + "rem";
+    answer.style.transition = "max-height 0.2s ease 0.2s";
+    answer.style.marginTop = ".8rem";
+    answer.style.opacity = 1;
   }
 }
 
